62dc554d1f59b2185db0f81276e5d856d590c5ac
Madikhan Sadykov <madikhansadykov@gmail.com>
2022-04-18 00:09:43 +0600

    first commit

---
+++ src/test/java/com/madikhan/demo/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madikhan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
